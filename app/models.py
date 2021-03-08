36a6f38eaf1ac610b51ef02efe43953ae6f2c32a
denis <mrisholigare@gmail.com>
2021-03-08 15:46:19 +0300

    downvote class added

--- app/models.py
+++ app/models.py
@@ -102,5 +102,25 @@ class Upvote(db.Model):
 
     def __repr__(self):
         return f'{self.user_id}:{self.pitch_id}'
+class Downvote(db.Model):
+    __tablename__ = 'downvotes'
 
+    id = db.Column(db.Integer,primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+    
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    @classmethod
+    def get_downvotes(cls,id):
+        downvote = Downvote.query.filter_by(pitch_id=id).all()
+        return downvote
+
+    def __repr__(self):
+        return f'{self.user_id}:{self.pitch_id}'
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
     
