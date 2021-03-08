eaac8ba3e08898de908b3b512cbc5ebaf9ff4b13
denis <mrisholigare@gmail.com>
2021-03-08 15:29:21 +0300

    importation is added

--- app/models.py
+++ app/models.py
@@ -1,27 +1,4 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    SQLALCHEMY_TRACK_MODIFICATIONS=True
-    SECRET_KEY='3d6f45a5fc12445dbac2f59c3b6c7cb1'
-    UPLOADED_PHOTOS_DEST ='app/static/photos'
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 465
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = True
-    MAIL_USERNAME = 'planetmoses12@gmail.com'
-    MAIL_PASSWORD = 'Lahaja40'
-
-class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-
-class DevConfig(Config):
-    DEBUG = True
-
-config_options = {
-'development':DevConfig,
-'production':ProdConfig
-}
-
+from . import db,login_manager
+from datetime import datetime
+from flask_login import UserMixin,current_user
+from werkzeug.security import generate_password_hash,check_password_hash
