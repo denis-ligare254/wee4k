5ba3d2d1cb9637678de934e9851c25b2cc3b7294
denis <mrisholigare@gmail.com>
2021-03-08 15:19:52 +0300

    class defined and importation done in manage.py and models.py

---
+++ app/models.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS=True
+    SECRET_KEY='3d6f45a5fc12445dbac2f59c3b6c7cb1'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = 'planetmoses12@gmail.com'
+    MAIL_PASSWORD = 'Lahaja40'
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
+
