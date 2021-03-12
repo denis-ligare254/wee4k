5d3e3a6caadd648217b750133ed34c346d502ceb
denis <mrisholigare@gmail.com>
2021-03-12 09:24:10 +0300

    init.py added

--- app/config.py
+++ app/config.py
@@ -12,7 +12,7 @@ class Config:
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
     MAIL_USERNAME = 'mrisholigare@gmail.com'
-    MAIL_PASSWORD = 'Lahaja40'
+    MAIL_PASSWORD = 'p@ssw0rd'
 
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
